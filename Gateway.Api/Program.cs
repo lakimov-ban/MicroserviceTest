07f9223c5088d11ca38bcd7ec942df69047d7b29
A_Lakimov <foreskhatabank@gmail.com>
2023-11-10 17:46:13 +0500

    dfdrt

--- Gateway.Api/Program.cs
+++ Gateway.Api/Program.cs
@@ -4,16 +4,34 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddEndpointsApiExplorer();
+// Add services to the container.
 
-builder.Configuration.AddJsonFile("ocelot.json",optional:false,reloadOnChange:true);
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 builder.Services.AddOcelot(builder.Configuration);
 
 var app = builder.Build();
 
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "app v1"));
+}
+
+
 app.UseMiddleware<AuthMiddleware>();
 
+app.UseHttpsRedirection();
 app.UseRouting();
+
+app.UseAuthorization();
+
 app.UseEndpoints(x => x.MapControllers());
 
 await app.UseOcelot();
