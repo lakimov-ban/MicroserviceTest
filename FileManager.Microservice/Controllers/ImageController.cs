e69653ffaa8701357f1c56b7622d26853eed4b96
A_Lakimov <foreskhatabank@gmail.com>
2023-11-22 17:47:53 +0500

    bjbjkfgjk

---
+++ FileManager.Microservice/Controllers/ImageController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FileManager.Microservice.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ImageController : ControllerBase
+{
+    private readonly string _imagesDirectory = "/app/images";
+
+    [HttpPost("upload")]
+    public async Task<IActionResult> UploadImage([FromForm] IFormFile file)
+    {
+        if (file == null || file.Length == 0)
+        {
+            return BadRequest("Invalid file");
+        }
+
+        try
+        {
+            string imageId = Guid.NewGuid().ToString();
+
+            string filePath = Path.Combine(_imagesDirectory, $"{imageId}.jpg");
+
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            ProcessImage(filePath);
+
+            return Ok(new { ImageId = imageId });
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"Internal server error: {ex.Message}");
+        }
+    }
+
+    private void ProcessImage(string filePath)
+    {
+        using (var image = Image.Load(filePath))
+        {
+            // Пример изменения размера изображения до 300x300 пикселей
+            image.Mutate(x => x.Resize(new ResizeOptions
+            {
+                Size = new Size(300, 300),
+                Mode = ResizeMode.Max
+            }));
+
+            // Сохранение измененного изображения
+            image.Save(filePath);
+        }
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetImage([FromRoute] string id)
+    {
+        try
+        {
+            // Построение пути к файлу изображения
+            string filePath = Path.Combine(_imagesDirectory, $"{id}.jpg");
+
+            // Проверка существования файла
+            if (!System.IO.File.Exists(filePath))
+            {
+                return NotFound("Image not found");
+            }
+
+            // Чтение файла изображения
+            var imageBytes = System.IO.File.ReadAllBytes(filePath);
+
+            // Определение MIME-типа изображения (может потребоваться дополнительная логика)
+            var mimeType = "image/jpeg";
+
+            // Возврат изображения в ответе
+            return File(imageBytes, mimeType);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"Internal server error: {ex.Message}");
+        }
+    }
+}
